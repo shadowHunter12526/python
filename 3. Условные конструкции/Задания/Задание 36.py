67cbdec67c476c956482e6d45d872d6f3a2334fe
Андрей Боев <a.boev.it@gmail.com>
2026-09-23 19:42:02 +0300

    Проверка ДЗ

--- 3. Условные конструкции/Задания/Задание 36.py
+++ 3. Условные конструкции/Задания/Задание 36.py
@@ -2,11 +2,14 @@ a = int(input())
 b = int(input())
 c = int(input())
 if a + b > c and a + c > b and b + c > a:
-    if a**2 + b**2 == c**2:
+    h = max(a, b, c)
+    c1 = min(a, b, c)
+    c2 = (a + b + c) - (h + c1)
+    if c1**2 + c2**2 == h**2:
         print("Прямоугольный")
-    elif a**2 + b**2 > c**2:
+    elif c1**2 + c2**2 > h**2:
         print("Остроугольный")
-    elif a**2 + b**2 < c**2:
+    elif c1**2 + c2**2 < h**2:
         print("Тупоугольный")
 else:
     print("Не существует")
--- 3. Условные конструкции/Задания/Задание 37.py
+++ 3. Условные конструкции/Задания/Задание 37.py
@@ -1,9 +1,19 @@
 n = int(input())
 if 11 <= n % 100 <= 14:
     print("грибов")
-elif n % 10 == 2 or n % 10 == 3 or n % 10 == 4:
+elif 2 >= n % 10 >= 4:
     print("гриба")
 elif n % 10 == 1:
     print("гриб")
 else:
     print("грибов")
+
+"""
+if 2 <= n % 10 <= 4 and not (12 <= n % 100 <= 14):
+    print("гриба")
+elif n % 10 == 1 and n % 100 != 11:
+    print("гриб")
+else:
+    print("грибов")
+
+"""
